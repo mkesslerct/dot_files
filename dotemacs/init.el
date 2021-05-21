af99392119357f619940a7d252a2c36366712ef8
Enrique Kessler Martínez <qkessler@gmail.com>
2021-05-21 10:25:58 +0200

    feat: add straight.el configuration
    In order to improve the reproducibility of my configuration files,
    I added the straight workflow, which besides creating lock files and
    saving the package versions for maximum similarity, improves the
    performance of my init file loading even further.

--- dotemacs/init.el
+++ dotemacs/init.el
@@ -1,29 +1,41 @@
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(package-initialize)
-(when (daemonp)
-    (exec-path-from-shell-initialize))
-(when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (setq comp-deferred-compilation t)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
+(straight-use-package 'use-package)
 (eval-when-compile
   (require 'use-package))
 
 (use-package auto-package-update
-  :ensure t
+  :straight t
+  :custom
+  (auto-package-update-delete-old-versions t)
+  (auto-package-update-hide-results t)
   :config
-  (setq auto-package-update-delete-old-versions t)
-  (setq auto-package-update-hide-results t)
   (auto-package-update-maybe))
+
+(use-package exec-path-from-shell
+  :straight t
+  :custom (exec-path-from-shell-arguments '("-l"))
+  :init 
+  (when (daemonp)
+    (exec-path-from-shell-initialize))
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
 (add-to-list 'load-path "~/.emacs.d/lisp/")
+(straight-use-package '(org :type built-in))
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
 (custom-set-variables
