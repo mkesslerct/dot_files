31fbd98184fb4f4252883d601acc079b8d8fad5d
Enrique Kessler Martínez <enrikes@amazon.com>
2021-09-08 08:58:36 +0200

    feat: add ack to org-roam-v2 in multiple places
    On the other hand, remove the raise emacs frame code, which was having
    problems with the gui script of emacs.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -64,18 +64,18 @@
 
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
-(when (featurep 'ns)
-  (defun ns-raise-emacs ()
-    "Raise Emacs."
-    (ns-do-applescript "tell application \"Emacs\" to activate"))
-  (defun ns-raise-emacs-with-frame (frame)
-    "Raise Emacs and select the provided frame."
-    (with-selected-frame frame
-      (when (display-graphic-p)
-        (ns-raise-emacs))))
-  (add-hook 'after-make-frame-functions 'ns-raise-emacs-with-frame)
-  (when (display-graphic-p)
-    (ns-raise-emacs)))
+;; (when (featurep 'ns)
+;;   (defun ns-raise-emacs ()
+;;     "Raise Emacs."
+;;     (ns-do-applescript "tell application \"Emacs\" to activate"))
+;;   (defun ns-raise-emacs-with-frame (frame)
+;;     "Raise Emacs and select the provided frame."
+;;     (with-selected-frame frame
+;;       (when (display-graphic-p)
+;;         (ns-raise-emacs))))
+;;   (add-hook 'after-make-frame-functions 'ns-raise-emacs-with-frame)
+;;   (when (display-graphic-p)
+;;     (ns-raise-emacs)))
 
 (setq custom-file "~/.emacs.d/var/custom.el")
 ;; (load custom-file)
